a4247a203221fe127f9de387c48cf81cd1dcc272
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-03 16:16:46 +0200

    feat(constants): add constants for db  and schema file paths

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+import path from 'node:path';
+
+export const DB_FILE_PATH = path.resolve(__dirname, '../db/urls.db');
+
+export const SCHEMA_FILE_PATH = path.resolve(__dirname, '../db/schema.sql');
