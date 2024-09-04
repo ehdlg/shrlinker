41a722d72bd504023199a8ec33c46c46c2b7e091
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-04 12:55:45 +0200

    feat: implement shorten route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
-import 'dotenv/config';
+import shortenRoute from './routes/shorten';
 import { handleError, notFound } from './middlewares';
+import 'dotenv/config';
 
 const app = express();
 const { PORT } = process.env;
+
+app.use(express.json());
+
+app.use('/shorten', shortenRoute);
 
 app.use(notFound);
 
