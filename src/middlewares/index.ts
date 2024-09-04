0c8a4ed4211e1d56361a35b9783045c9b3495a43
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-04 09:16:50 +0200

    feat(middlewares): add add centralized error-handling middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,6 +1,13 @@
-import { RequestHandler } from 'express';
+import { ErrorRequestHandler, RequestHandler } from 'express';
 import HttpError from '../errors/HttpError';
 
 export const notFound: RequestHandler = () => {
   throw new HttpError({ status: 404, message: 'Not found' });
+};
+
+export const handleError: ErrorRequestHandler = (error, _req, res, _next) => {
+  const status = error.status || 500;
+  const message = error.message || 'Something went wrong';
+
+  return res.status(status).json({ error: message });
 };
