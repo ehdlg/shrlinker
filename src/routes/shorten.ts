259bcb063ce199366dca74c9cb27f27c48c4f5d9
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-04 16:27:56 +0200

    feat(controller): add /shorten PUT endpoint

--- src/routes/shorten.ts
+++ src/routes/shorten.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express';
-import { createUrl, getAllUrl, getUrl, deleteUrl } from '../controllers/ShortenController';
+import {
+  createUrl,
+  getAllUrl,
+  getUrl,
+  deleteUrl,
+  updateUrl,
+} from '../controllers/ShortenController';
 import { deleteByCode } from '../models/Shorten';
 
 const router = Router();
@@ -7,6 +13,8 @@ const router = Router();
 router.get('/:shortCode', getUrl);
 
 router.delete('/:shortCode', deleteUrl);
+
+router.put('/:shortCode', updateUrl);
 
 router.get('/', getAllUrl);
 
