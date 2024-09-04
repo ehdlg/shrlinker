4f19bc0e225103728047d4afd03f39c34607d7db
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-04 15:37:03 +0200

    feat(routes): add /shorten DELETE endpoint

--- src/routes/shorten.ts
+++ src/routes/shorten.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import { createUrl, getAllUrl, getUrl } from '../controllers/ShortenController';
+import { createUrl, getAllUrl, getUrl, deleteUrl } from '../controllers/ShortenController';
+import { deleteByCode } from '../models/Shorten';
 
 const router = Router();
 
 router.get('/:shortCode', getUrl);
+
+router.delete('/:shortCode', deleteUrl);
 
 router.get('/', getAllUrl);
 
