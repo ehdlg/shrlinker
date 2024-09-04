0704c458c99584ad9a1490ec3474306610bbd345
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-04 10:14:00 +0200

    feat(constants): add letter range constant

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,8 @@ import path from 'node:path';
 export const DB_FILE_PATH = path.resolve(__dirname, '../db/urls.db');
 
 export const SCHEMA_FILE_PATH = path.resolve(__dirname, '../db/schema.sql');
+
+const FIRST_LETTER = 'A';
+const LAST_LETTER = 'z';
+
+export const LETTER_RANGE = LAST_LETTER.charCodeAt(0) - FIRST_LETTER.charCodeAt(0);
