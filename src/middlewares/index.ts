25dc886cf140f8c8c2ab304aa877cf61b9abbbcc
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-05 09:15:07 +0200

    feat(middlewares): add validate middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,5 +1,6 @@
 import { ErrorRequestHandler, RequestHandler } from 'express';
 import HttpError from '../errors/HttpError';
+import { validationResult } from 'express-validator';
 
 export const notFound: RequestHandler = () => {
   throw new HttpError({ status: 404, message: 'Not found' });
@@ -10,4 +11,16 @@ export const handleError: ErrorRequestHandler = (error, _req, res, _next) => {
   const message = error.message || 'Something went wrong';
 
   return res.status(status).json({ error: message });
+};
+
+export const validate: RequestHandler = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (errors.isEmpty()) return next();
+
+  const errorMessages = errors.array().map((error) => {
+    return error.msg;
+  });
+
+  return res.status(422).json({ errors: errorMessages });
 };
