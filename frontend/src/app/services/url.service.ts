2b8c6ec914d83233b297848e7602bd04e3c7e7c3
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-05 12:00:37 +0200

    feat(services): add create function to post a new url

--- frontend/src/app/services/url.service.ts
+++ frontend/src/app/services/url.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { URL } from '../types';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -11,6 +11,21 @@ export class UrlService {
   constructor(private http: HttpClient) {}
 
   get(shortCode: string): Observable<URL> {
-    return this.http.get<URL>(`${environment.apiUrl}/${shortCode}`);
+    return this.http
+      .get<URL>(`${environment.apiUrl}/${shortCode}`)
+      .pipe(take(1));
+  }
+
+  create(url: string): Observable<URL> {
+    const body = {
+      url,
+    };
+    return this.http
+      .post<URL>(environment.apiUrl, body, {
+        headers: {
+          'Content-type': 'application/json',
+        },
+      })
+      .pipe(take(1));
   }
 }
