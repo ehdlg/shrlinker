deb35d7adec5997b1b5922d54e3046d36befaa1d
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-05 10:41:13 +0200

    feat(routes): implement GET stats endpoint

--- backend/src/routes/shorten.ts
+++ backend/src/routes/shorten.ts
@@ -5,11 +5,14 @@ import {
   getUrl,
   deleteUrl,
   updateUrl,
+  getStats,
 } from '../controllers/ShortenController';
 import { shortUrlRule, urlRule } from '../validation';
 import { validate } from '../middlewares';
 
 const router = Router();
+
+router.get('/:shortCode/stats', shortUrlRule, validate, getStats);
 
 router.get('/:shortCode', shortUrlRule, validate, getUrl);
 
