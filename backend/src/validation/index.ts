eb858ca143ffdf65a86d7ccce2a3433572424241
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-08 11:49:36 +0200

    feat: update invalid url validation message

--- backend/src/validation/index.ts
+++ backend/src/validation/index.ts
@@ -2,14 +2,7 @@ import { body, param } from 'express-validator';
 import { SHORT_URL_REGEXP } from '../constants';
 
 export const urlRule = (() => {
-  return [
-    body('url')
-      .exists()
-      .withMessage('The `url` property is required')
-      .bail()
-      .isURL()
-      .withMessage('The `url` property must be a valid URL'),
-  ];
+  return [body('url').exists().bail().isURL().withMessage('Invalid URL')];
 })();
 
 export const shortUrlRule = (() => {
