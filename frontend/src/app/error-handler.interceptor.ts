91798705fbad787a2b4ef5511c8eed9191b6dba0
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-08 11:19:37 +0200

    feat(interceptor): update error cases scenarios

--- frontend/src/app/error-handler.interceptor.ts
+++ frontend/src/app/error-handler.interceptor.ts
@@ -3,9 +3,11 @@ import { inject } from '@angular/core';
 import { ErrorService } from './services/error.service';
 import { ErrorResponse } from './types';
 import { catchError, EMPTY } from 'rxjs';
+import { Router } from '@angular/router';
 
 export const errorHandlerInterceptor: HttpInterceptorFn = (req, next) => {
   const errorService = inject(ErrorService);
+  const router = inject(Router);
 
   errorService.clearErrors();
 
@@ -16,6 +18,15 @@ export const errorHandlerInterceptor: HttpInterceptorFn = (req, next) => {
       switch (error.status) {
         case 400:
           errorMessage = error.error.error ?? 'Something went wrong';
+          break;
+
+        case 404:
+          errorMessage = 'Short URL not found';
+          break;
+
+        case 422:
+          errorMessage =
+            error.error.errors?.join(', ') ?? 'Something went wrong';
           break;
 
         case 500:
